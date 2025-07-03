b7f209f6ef0b221dc42ae66fa33812d6155a29d9
danicolms <danicolms@icloud.com>
2025-07-03 15:16:54 +0200

    feat: center logo

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import logo from '/logo.jpeg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <a className="logo__link" href="#" target="_blank">
+          <img src={logo} className="logo__img" alt="Limerencia: La lucha por conquistar el amor propio" />
+        </a>
+      </div>
+    </>
+  )
+}
+
+export default App
