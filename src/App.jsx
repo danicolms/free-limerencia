bac398be5b40d11a7c2260af98c09cdf572ccc00
danicolms <danicolms@icloud.com>
2025-07-03 15:40:07 +0200

    chore: limit by country

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,53 @@
 import logo from '/logo.jpeg'
 import './App.css'
+import { useEffect, useState } from 'react'
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchIPData = async () => {
+      try {
+        const response = await fetch('https://free.freeipapi.com/api/json');
+        if (!response.ok) {
+          throw new Error('Failed to fetch IP data');
+        }
+        const data = await response.json();
+        const countryCode = data.countryCode
+
+        if(countryCode !== "ES"){
+          throw new Error("Esta página está habilitada solo para España")
+        }
+
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchIPData();
+  }, []);
+
+
+  if(loading) return <h1> Cargando ... </h1>
+  
+  if(error) return <h1> {error} </h1>
+
   return (
     <>
-      <div>
-        <a className="logo__link" href="#" target="_blank">
+        <a className="logo__link">
           <img src={logo} className="logo__img" alt="Limerencia: La lucha por conquistar el amor propio" />
         </a>
-      </div>
+
+        <p> Viva Venezuela mi patria querida, que liberto mi hermano fue Simon Bolivar</p>
+
+        <button> Descargar gratis </button>
+
+        <section>
+          <a> Me invitas un café? </a>
+        </section>
     </>
   )
 }
