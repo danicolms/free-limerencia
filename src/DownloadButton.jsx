63afd2c0e00c36bc316c01181f3339031dca6f0f
danicolms <danicolms@icloud.com>
2025-07-09 11:06:14 +0200

    feat: font and style button accordingly

--- src/DownloadButton.jsx
+++ src/DownloadButton.jsx
@@ -1,3 +1,20 @@
-export default function DownloadButton({download, isLoading}){
-    return <button className="download__button" onClick={download}> {isLoading ? "..." : "Descargar gratis"} </button>
+export default function DownloadButton({download, isLoading = false, hasDownloaded = false}){
+    let caption;
+    let classes = "download__button";
+
+    if(hasDownloaded) {
+        caption = "¡Que lo disfrutes!"
+        classes += " download__button--downloaded"
+    }
+
+    if(isLoading) {
+        caption = "Descargando..."
+        classes += " download__button--loading"
+    }
+
+    if(!isLoading && !hasDownloaded) {
+        caption = "~ Descargar gratis ~"
+    }
+
+    return <button className={classes} onClick={download}> {caption} </button>
 }
